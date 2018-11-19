6c09ddfeb3a96ba29fa39a7a628b36ab4fae11db
david mulligan <david@davids-MacBook-Pro.local>
2018-11-19 21:50:17 +0000

    setup

---
+++ number/toOrdinal.cs
@@ -0,0 +1,12 @@
+private static string[] singles = {"", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"};
+private static string[] teens   = {"", "eleven", "tweleve", "thir", "", "fiv"};
+private static string[] tens    = {"ten", "twenty"};
+private static string[] suffies = {"teen", "ty", "hundred"};
+private static string   nil     = "zero";
+
+private static string[] denominations = {"thousand", "million", "billion", "trillion"};
+
+
+public static string ToOrdinal(int number) {
+
+}
